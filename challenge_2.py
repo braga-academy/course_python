6f876e8d354a8dbceb191ef0f6fff934435243d8
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-08-09 22:36:28 -0300

    Create challenge 2

---
+++ challenge_2.py
@@ -0,0 +1,4 @@
+day     = input('Em que dia você Nasceu? ')
+month   = input('De que mês? ')
+year    = input('em que ano? ')
+print('Você nasceu em ' + day + ' de ' + month + ' de ' + year + '. Correto?')
