51d205e721fb05bb630158d951074a895d8b2aa1
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-08-09 22:56:15 -0300

    Create challenge 3

---
+++ challenge_3.py
@@ -0,0 +1,6 @@
+print('Vamos Fazer uma soma?')
+number_one = input('Digite o primeiro Número: ')
+number_two = input('Digite o segundo Número: ')
+
+result = float(number_one) + float(number_two) # float() permite que sejam inseridos números decimais
+print (result)
