e57ce79169baa2eb04b38844e275319193b04064
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-08-09 22:23:09 -0300

    Initial Commit

---
+++ desafio_1.py
@@ -0,0 +1,5 @@
+nome = input('Olá, Qual é o seu nome?')
+print('Olá ' + nome + ', é um prazer te conhecer!!')
+idade = input('Qual sua idade ' + nome + '?')
+peso = input('Legal. E qual o seu peso ' + nome + '?')
+print('Muito bom! \n ' + 'Meu nome é Luciano Braga e criei este script em Python no curso do Gustavo Guanabara')
